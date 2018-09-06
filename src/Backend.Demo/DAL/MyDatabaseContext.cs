aafca32c3531f12fa25d874922c251a510a73332
kennyflores <35266716+kennyflores@users.noreply.github.com>
2018-09-06 14:44:47 -0600

    Sept 6 Activity

---
+++ src/Backend.Demo/DAL/MyDatabaseContext.cs
@@ -0,0 +1,19 @@
+﻿using Backend.Demo.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Demo.DAL
+{
+    internal class MyDatabaseContext : DbContext
+    {
+        public MyDatabaseContext() : base("name=AdHocDb")
+        {
+
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
